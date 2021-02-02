6134e58567e4ffd07074c8fe0f338fa139ade4f3
jos <jos@josvromans.com>
2021-02-02 02:40:54 +0100

    Added the WebChaos app from another repository

---
+++ webchaos/README.md
@@ -0,0 +1,66 @@
+# Chaos Game - Desktop Browser App
+You can play with this app in your web browser, preferably on a laptop / desktop:
+
+https://josvromans.github.io/chaosgame/
+
+
+In addition to the regular Chaos Game (<a href="https://www.youtube.com/watch?v=kbKtFN71Lfs">explanation on YouTube</a>),
+you can add several polygons / lines / points that interplay with each other.
+
+**Parameters**
+
+Background color: sets the canvas background color
+
+
+Color: sets the color of the current polygon, at the moment when &lt;space&gt; is hit (see the controls section)
+
+
+Switch: the number of iterations after which the Chaos Game will switch from the current polygon to another polygon.
+
+
+Divisor: when the divisor is 2, every next point will be drawn halfway the previous point, and the selected random corner point.
+
+
+Strategy: The 'random' option will switch to a random nexy polygon, where every polygon (even the current one) has the same chance.
+The 'next' option will always switch to the next polygon (in order of addition), and after the last polygon, the next one will be the first polygon.
+
+
+**Controls**
+
+Mouse click (or finger on tablet/phone) to add single points on the canvas.
+
+
+&lt;space&gt; to make a closed polygon of all the points so far.
+To add more polygons, add more points and hit &lt;space&gt; again.
+To add a single point, you have to hit &lt;space&gt; after a single mouse click.
+Every time when you hit &lt;space&gt; the polygon will be drawn in the selected 'color'. So you can make something colorful by selecting a different color every time.
+There is a button 'add polygon' with the same effect (for if you are on a device without a keyboard).
+
+
+&lt;enter&gt; to start the chaos game. This can take a few seconds.
+
+
+&lt;F5&gt; To start over with a blank canvas (by just refreshing the page)
+
+
+<ctrl + c> will copy the list of polygons to the clipboard. So this can also be used as a tool to 'hand draw' some figures, and then use the coordinates in another program.
+The structure is a Python compatible list of lists of tuples (where each tuple is a point, each list of tuples is a polygon, and the whole is a list of polygons).
+For example:
+
+```python
+polygon_list = [
+    [
+        (100, 100),
+        (500, 100),
+        (500, 500),
+    ],
+]
+```
+
+**A few examples**
+
+![Example 1](example_1.png)
+![Example 2](example_2.png)
+![Example 3](example_3.png)
+![Example 4](example_4.png)
+![Example 5](example_5.png)
---
+++ webchaos/chaosgame.js
@@ -0,0 +1,163 @@
+var polygon_list = [];
+var temp_coordinates_list = [];
+var colors = [];
+var block_action = false;
+var iterations = 1500000;
+
+
+function copyToClipboard(clip_str) {
+    var el = document.createElement('textarea');
+    el.value = clip_str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+
+// This function always returns a random number between min and max (both included):
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function getRandomItem(list) {
+    var random_index = getRandomInteger(0, list.length - 1);
+    return list[random_index];
+}
+
+function draw_polygon(canvas){
+    if (block_action === true) {
+        return;
+    }
+    if (temp_coordinates_list.length === 0){
+        return;
+    }
+
+    block_action = true;
+
+    var color = document.getElementById('current_color').value;
+    colors.push(color);
+
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+
+    for (var item=0; item<temp_coordinates_list.length ; item+=1 ){
+        ctx.lineTo(temp_coordinates_list[item][0], temp_coordinates_list[item][1]);
+    }
+
+    ctx.closePath();
+
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 1;
+    ctx.stroke();
+
+    polygon_list.push(temp_coordinates_list);
+
+    temp_coordinates_list = [];
+    block_action = false;
+}
+
+
+function clear_canvas(ctx, canvas){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    polygon_list = [];
+    temp_coordinates_list = [];
+    colors = [];
+}
+
+
+function start_chaos_game(ctx, canvas){
+    if (block_action === true) {
+        return;
+    }
+    if (polygon_list.length === 0){
+        return;
+    }
+    block_action = true;
+
+    var bg_color = document.getElementById('background_color').value;
+
+    ctx.fillStyle = bg_color;
+    canvas.style.backgroundColor = bg_color;
+    document.body.style.background = bg_color;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    var switch_poly = document.getElementById('switch_poly').value;
+    var switch_strategy = document.getElementById('switch-strategy').value;
+    var divisor = parseFloat(document.getElementById('divisor').value);
+
+    var polygon_index = 0;
+    var color = colors[polygon_index];
+    var start_point = [0, 0];
+
+    for (var i=0; i<iterations; i+=1) {
+        if (i % switch_poly === 0) {
+            if (switch_strategy === 'random') {
+                polygon_index = getRandomInteger(0, polygon_list.length - 1);
+            } else {
+                polygon_index = (polygon_index + 1) % polygon_list.length;
+            }
+            color = colors[polygon_index % colors.length];
+        }
+
+        var random_point = getRandomItem(polygon_list[polygon_index]);
+        var midpoint = [
+            start_point[0] + (random_point[0] - start_point[0]) / divisor,
+            start_point[1] + (random_point[1] - start_point[1]) / divisor,
+        ];
+
+        ctx.fillStyle = color;
+        ctx.fillRect(midpoint[0], midpoint[1], 1, 1);
+
+        start_point = midpoint;
+    }
+    block_action = false;
+}
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    var canvas = document.getElementById('canvas');
+    canvas.width  = window.innerWidth - 4;
+    canvas.height = window.innerHeight - 4;
+    var ctx = canvas.getContext('2d');
+
+    // Add points on mouse down
+    canvas.addEventListener("mousedown", function(event){
+        var bound = canvas.getBoundingClientRect();
+        var x = event.clientX - bound.left - canvas.clientLeft;
+        var y = event.clientY - bound.top - canvas.clientTop;
+        temp_coordinates_list.push([x, y]);
+
+        ctx.fillStyle = '#FFF';
+        ctx.fillRect(x - 1,y - 1,2,2);
+    });
+
+    document.getElementById('add_polygon').addEventListener("mouseup", function() {
+        draw_polygon(canvas);
+    });
+    document.getElementById('start').addEventListener("mouseup", function() {
+        start_chaos_game(ctx, canvas);
+    });
+    document.getElementById('clear').addEventListener("mouseup", function() {
+        clear_canvas(ctx, canvas);
+    });
+
+    document.addEventListener('keydown', function(event) {
+        // on ctrl-c: add the list of polygons to the clipboard (as a Python list of lists of tuples)
+        if (event.code == 'KeyC' && (event.ctrlKey || event.metaKey)) {
+            var clip_str = 'polygon_list = [\n';
+            for (var poly_index=0; poly_index<polygon_list.length; poly_index+=1 ){
+                clip_str += '    [\n';
+                for (var index=0; index<polygon_list[poly_index].length; index+=1 ){
+                    clip_str += '        (' + polygon_list[poly_index][index][0] + ', ' + polygon_list[poly_index][index][1] + '),\n';
+                }
+                clip_str += '    ],\n';
+            }
+            clip_str += ']';
+            copyToClipboard(clip_str);
+        }
+        else if (event.keyCode === 13) {start_chaos_game(ctx, canvas);}  // Enter
+        else if (event.code === 'Space') {draw_polygon(canvas);}
+        else if (event.keyCode === 67) {clear_canvas(ctx, canvas);}  // c
+    });
+});
